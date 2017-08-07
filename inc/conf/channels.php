f13bf70c0651af7ae656ef28e2ef9d39c3823b0e
DeaVulTan <deavultan@gmail.com>
2017-08-08 04:03:25 +0800

    source

---
+++ inc/conf/channels.php
@@ -0,0 +1,74 @@
+<?php $channels = array (
+  0 => 
+  array (
+    'name' => 'NRG Radio Tas-IX',
+    'logo' => 'tmp/images/logo.1448823739.jpg',
+    'skin' => 'basic.dark.css',
+    'show-time' => true,
+    'streams' => 
+    array (
+      'Default Quality' => 
+      array (
+        'mp3' => 'http://melody.uz:8003/energy',
+      ),
+    ),
+    'stats' => 
+    array (
+      'method' => 'disabled',
+    ),
+  ),
+  1 => 
+  array (
+    'name' => 'Record Tas-Ix',
+    'logo' => 'tmp/images/logo.1448821754.jpg',
+    'skin' => 'basic.dark.css',
+    'show-time' => true,
+    'streams' => 
+    array (
+      'Default Quality' => 
+      array (
+        'mp3' => 'http://melody.uz:8003/radiorecord',
+      ),
+    ),
+    'stats' => 
+    array (
+      'method' => 'disabled',
+    ),
+  ),
+  2 => 
+  array (
+    'name' => 'DFM Tas-Ix',
+    'logo' => 'tmp/images/logo.1449000508.jpg',
+    'skin' => 'basic.dark.css',
+    'show-time' => true,
+    'streams' => 
+    array (
+      'Default Quality' => 
+      array (
+        'mp3' => 'http://melody.uz:8003/dfm',
+      ),
+    ),
+    'stats' => 
+    array (
+      'method' => 'disabled',
+    ),
+  ),
+  3 => 
+  array (
+    'name' => 'Europa Plus Tas-Ix',
+    'logo' => 'tmp/images/logo.1449000720.jpg',
+    'skin' => 'basic.dark.css',
+    'show-time' => true,
+    'streams' => 
+    array (
+      'Default Quality' => 
+      array (
+        'mp3' => 'http://melody.uz:8003/europa',
+      ),
+    ),
+    'stats' => 
+    array (
+      'method' => 'disabled',
+    ),
+  ),
+); ?>
---
+++ inc/lang/Ru.php
@@ -0,0 +1,16 @@
+<?php $lang=array (
+  'loading-message' => 'Загружка Пожалуйста Подождите',
+  'ui-settings' => 'Выберите Качество',
+  'ui-channels' => 'Список Каналов',
+  'ui-play' => 'Начать Воспроизведение',
+  'ui-stop' => 'Остановить воспроизведение',
+  'ui-volume-circle' => 'Перетащите Чтобы изменить Громкость',
+  'ui-playlists' => 'Слушайте В своем любимом Плеере',
+  'status-init' => 'Загрузка потока {STREAM}',
+  'status-playing' => 'Проигрывание потока {STREAM}',
+  'status-stopped' => 'Плеер Остановлен',
+  'status-volume' => 'Громкость: {LEVEL}',
+  'status-muted' => 'Плеер приглушен',
+  'share' => 'Отправить',
+  'twitter-share' => 'Я слушаю {TRACK}!',
+); ?>
